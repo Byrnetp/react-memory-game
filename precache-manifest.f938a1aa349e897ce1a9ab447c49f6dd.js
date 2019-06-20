ac1648289a5a2e4193a27b037785e98ef0677d44
Travis Byrne <travis.p.byrne@gmail.com>
2019-06-19 23:28:02 -0600

    Updates

---
+++ precache-manifest.f938a1aa349e897ce1a9ab447c49f6dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08995236e5dadc3bba5f863fb8d54efd",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "1c5cd6a68c76e5127dd3",
+    "url": "/react-memory-game/static/css/main.3e7c7d0d.chunk.css"
+  },
+  {
+    "revision": "5fca2c286303de9d5954",
+    "url": "/react-memory-game/static/js/2.e2bc4550.chunk.js"
+  },
+  {
+    "revision": "1c5cd6a68c76e5127dd3",
+    "url": "/react-memory-game/static/js/main.6eed9a39.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
